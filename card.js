0384aae6ae70e3759ef7437adb8c45fc14cec5cf
Nataponchobjai <nataponchobjai@gmail.com>
2023-10-26 17:12:57 -0500

    2

--- card.js
+++ card.js
@@ -1,49 +1,52 @@
-async function fetchCards() {
+
+async function fetchAtkCards() {
   try {
-      
       const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php');
       const data = await response.json();
-
-      
-      let cards = data.data;
-
-      
-      const minimumAtk = 0;
-      cards = cards.filter(card => card.atk >= minimumAtk);
-
-      
-      const cardListElement = document.getElementById('card-list');
-
-      
-      cardListElement.innerHTML = '';
-
-      cards.forEach(card => {
-          
-          const cardDiv = document.createElement('div');
-          cardDiv.classList.add('card-container');
-          
-         
-          const cardName = document.createElement('h2');
-          cardName.textContent = card.name;
-          cardDiv.appendChild(cardName);
-
-          
-          if (card.card_images) {
-              card.card_images.forEach(image => {
-                  const img = document.createElement('img');
-                  img.src = image.image_url;
-                  img.alt = card.name;
-                  cardDiv.appendChild(img);
-              });
-          }
-
-         
-          cardListElement.appendChild(cardDiv);
-      });
+      const cards = data.data.filter(card => card.atk >= 1500); 
+      displayCards(cards);
   } catch (error) {
-      console.error('Error fetching the cards:', error);
+      console.error('Error fetching the attack cards:', error);
   }
 }
 
 
-document.getElementById('load-cards-button').addEventListener('click', fetchCards);
+async function fetchSpellCards() {
+  try {
+      const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Spell Card');
+      const data = await response.json();
+      displayCards(data.data);
+  } catch (error) {
+      console.error('Error fetching the spell cards:', error);
+  }
+}
+
+
+function displayCards(cards) {
+  const cardListElement = document.getElementById('card-list');
+  cardListElement.innerHTML = '';
+
+  cards.forEach(card => {
+      const cardDiv = document.createElement('div');
+      cardDiv.classList.add('card-container');
+
+      const cardName = document.createElement('h2');
+      cardName.textContent = card.name;
+      cardDiv.appendChild(cardName);
+
+      if (card.card_images) {
+          card.card_images.forEach(image => {
+              const img = document.createElement('img');
+              img.src = image.image_url;
+              img.alt = card.name;
+              cardDiv.appendChild(img);
+          });
+      }
+
+      cardListElement.appendChild(cardDiv);
+  });
+}
+
+
+document.getElementById('load-attack-cards-button').addEventListener('click', fetchAtkCards);
+document.getElementById('load-spell-cards-button').addEventListener('click', fetchSpellCards);
